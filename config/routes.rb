00b9f65c1135aa0f5665ea810693f47e56fbd10e
Stewart <stewart.mckinlay@gmail.com>
2021-02-04 18:11:33 -0700

    Implement User Reg and Login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  get '/about' => 'about#index'
+  resources :about, only: [:index]
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
