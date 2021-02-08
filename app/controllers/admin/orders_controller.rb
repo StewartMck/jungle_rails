936e7ed9e01baa7df6fe7ccafe45c7d7432d2840
Stewart <stewart.mckinlay@gmail.com>
2021-02-07 20:09:30 -0700

    Implement detailed orders view

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,7 +5,18 @@ class Admin::OrdersController < Admin::BaseController
   end
 
   def show
-    @items = LineItem.joins(:product).where({order_id: params[:id]}).pluck(:product_id, :name, :quantity, :item_price_cents, :total_price_cents)
+    @items = LineItem.joins(:product).where({order_id: params[:id]}).select(:order_id, :product_id, :name, :quantity, :item_price_cents, :total_price_cents)
+    @order = Order.find(params[:id])
   end
+
+  def discount?(cost_price, paid_price)
+    if cost_price.instance_of? (Array)
+      total_cost = cost_price.reduce(0) { |sum, n| sum + n}
+      return total_cost - paid_price.to_i
+    else
+    return cost_price - paid_price.to_i
+  end
+end
+helper_method :discount?
 
 end
