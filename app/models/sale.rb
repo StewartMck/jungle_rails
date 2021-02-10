ad1e56f60f668c26a40f735b5394915cdc91c135
Stewart <stewart.mckinlay@gmail.com>
2021-02-10 06:57:44 -0700

    Validate sales dates

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,10 +1,12 @@
 class Sale < ActiveRecord::Base
+  validates :starts_on, :ends_on, presence: true
+  validate :end_date_after_start_date
 
   # AR Scope
   # scope :active, => {where(.....)}
   def self.active
-    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
-  end  
+    where('sales.starts_on <= ? AND sales.ends_on >= ?', Date.current, Date.current)
+  end
 
   def finished?
     ends_on < Date.current
@@ -18,4 +20,13 @@ class Sale < ActiveRecord::Base
     !upcomming? && !finished?
   end
 
+  private
+
+  def end_date_after_start_date
+    if ends_on < starts_on
+      errors.add(:ends_on, 'must be after the start date')
+    elsif starts_on < Date.current
+      errors.add(:starts_on, 'must be a future date')
+    end
+end
 end
