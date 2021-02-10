b028b8244de1d4af4b34524b1a86c228d774e9aa
Stewart <stewart.mckinlay@gmail.com>
2021-02-09 17:08:07 -0700

    Validate stock ordered with stock on hand

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,4 +6,13 @@ class LineItem < ActiveRecord::Base
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
 
+  validate :stock_on_hand
+
+  def stock_on_hand
+    product = Product.find_by_id(self.product_id)
+    if product.quantity < self.quantity
+      errors.add(:base , "Stock Insufficient => #{product.name} stock on hand: #{product.quantity}")
+    end
+end
+
 end
